2d2dc767feb0e3802c7de1381c4ed3beaf8962f3
Dagoranna <icywizard1@gmail.com>
2021-02-18 12:26:47 +0200

    second

--- server_4.js
+++ server_4.js
@@ -1,5 +1,7 @@
-constWebSocket = require('ws')
-const wss = newWebSocket.Server({ port: 8080 })
+constWebSocket = require('ws');
+const PORT = process.env.PORT || 80;
+const wss = newWebSocket.Server({ port: PORT });
+
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
