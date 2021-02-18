bf5cd093d4995aa64fdd337460172d1dd25738d9
Dagoranna <icywizard1@gmail.com>
2021-02-18 12:46:47 +0200

    reflex added

--- server_4.js
+++ server_4.js
@@ -4,7 +4,8 @@ const wss = new WebSocket.Server({ port: PORT });
 
 wss.on('connection', ws => {
   ws.on('message', message => {
-    console.log(`Received message => ${message}`)
+    console.log(`Received message => ${message}`);
+	ws.send('I got ' + message);
   })
   ws.send('ho!')
 })
