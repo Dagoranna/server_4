d87ffc827fa89138dc486914bbf89c2adecdcfb4
Dagoranna <icywizard1@gmail.com>
2021-02-19 12:17:34 +0200

    dice roll 1

--- server_4.js
+++ server_4.js
@@ -5,8 +5,12 @@ const wss = new WebSocket.Server({ port: PORT });
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`);
-	let mydata = message * 2;
-	ws.send('I got ' + mydata);
+	//message состоит из айди игры, имени игрока, типа кубика, кол-ва бросков
+	//например: 23452|Icy|20|1
+	
+	let messageArr = message.split('|'); 
+	let randInt = Math.random() * (messageArr[2] - 1) + 1;
+	ws.send(messageArr[1] + ', you got ' + randInt 'on d' + messageArr[2] ); //FOR TEST
   })
   ws.send('server_awakened')
 })
