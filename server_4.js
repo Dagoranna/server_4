6fc9e8deb63ee8301956cfa11a320c888f0bec76
Dagoranna <icywizard1@gmail.com>
2021-02-19 15:50:52 +0200

    ближе к чату 1

--- server_4.js
+++ server_4.js
@@ -1,6 +1,12 @@
-const WebSocket = require('ws');
+const ws = new require('ws');
 const PORT = process.env.PORT || 80;
 const wss = new WebSocket.Server({ port: PORT });
+
+const gamers = new Set();
+
+http.createServer((req, res) => {
+	wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+});
 
 //функция полидайс
 function polydice(dice,diceNumber){
@@ -10,8 +16,31 @@ function polydice(dice,diceNumber){
 	}	
 	return rolls;
 }
+
+function onSocketConnect(ws) {
+  gamers.add(ws);
+  ws.on('message', function(message) {
+	//message состоит из запрашиваемой функции, айди игры, имени игрока, типа кубика, кол-ва бросков
+	//например: dice|23452|Icy|20|2
+	let answer;
+	//answer возвращает исходное сообщение, добавляя к нему результаты бросков через '|'
+	//например: dice|23452|Icy|20|2|15|18
+	let messageArr = message.split('|');
+	if (messageArr[0] == 'dice'){
+		answer = message + polydice(messageArr[3],messageArr[4]);			
+	}
+		
+    for(let gamer of gamers) {
+		gamer.send(answer);
+    }
+  });
+
+  ws.on('close', function() {
+    gamers.delete(ws);
+  });
+}
 	
-wss.on('connection', ws => {
+/*wss.on('connection', ws => {
 	ws.on('message', message => {
 		console.log(`Received message => ${message}`);
 		//message состоит из запрашиваемой функции, айди игры, имени игрока, типа кубика, кол-ва бросков
@@ -27,4 +56,4 @@ wss.on('connection', ws => {
 		}
 	})
 	ws.send('server_awakened');
-})
+})*/
