9215a671ac9b26b85b8be1a52a7de6f1b09a948d
Dagoranna <icywizard1@gmail.com>
2021-02-19 13:00:04 +0200

    dice to function

--- server_4.js
+++ server_4.js
@@ -2,21 +2,29 @@ const WebSocket = require('ws');
 const PORT = process.env.PORT || 80;
 const wss = new WebSocket.Server({ port: PORT });
 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`);
-	//message состоит из айди игры, имени игрока, типа кубика, кол-ва бросков
-	//например: 23452|Icy|20|1
-	let answer = message;
-	let messageArr = message.split('|'); 
-	let randInt;
-	for(let i = 1; i <= messageArr[3]; i++){
-		randInt = Math.round(Math.random() * (messageArr[2] - 1) + 1);
-		answer = answer + '|' + randInt;
-	}
+//функция полидайс
+function polydice(dice,diceNumber){
+	let rolls;
+	for(let i = 1; i <= diceNumber; i++){
+		rolls = rolls + '|' + Math.round(Math.random() * (dice - 1) + 1);
+	}	
+	return rolls;
+}
 	
-	//ws.send(messageArr[1] + ', you got ' + randInt + ' on d' + messageArr[2] ); //FOR TEST
-	ws.send(answer);
-  })
-  ws.send('server_awakened');
+wss.on('connection', ws => {
+	ws.on('message', message => {
+		console.log(`Received message => ${message}`);
+		//message состоит из запрашиваемой функции, айди игры, имени игрока, типа кубика, кол-ва бросков
+		//например: dice|23452|Icy|20|2
+		let answer;
+		//answer возвращает исходное сообщение, добавляя к нему результаты бросков через '|'
+		//например: dice|23452|Icy|20|2|15|18
+		let messageArr = message.split('|'); 
+		
+		if (messageArr[0] == 'dice'){
+			answer = message + polydice(messageArr[3],messageArr[4]);
+			ws.send(answer);
+		}
+	})
+	ws.send('server_awakened');
 })
