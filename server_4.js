66baa71d4cd728e8023a1ff92e30515be792270f
Dagoranna <icywizard1@gmail.com>
2021-02-19 12:31:40 +0200

    добавлена возможность нескльких бросков

--- server_4.js
+++ server_4.js
@@ -7,10 +7,16 @@ wss.on('connection', ws => {
     console.log(`Received message => ${message}`);
 	//message состоит из айди игры, имени игрока, типа кубика, кол-ва бросков
 	//например: 23452|Icy|20|1
-	
+	let answer = message;
 	let messageArr = message.split('|'); 
-	let randInt = Math.round(Math.random() * (messageArr[2] - 1) + 1);
-	ws.send(messageArr[1] + ', you got ' + randInt + 'on d' + messageArr[2] ); //FOR TEST
+	let randInt;
+	for(let i = 1; i < messageArr[3]; i++){
+		randInt = Math.round(Math.random() * (messageArr[2] - 1) + 1);
+		answer = answer + '|' + randInt;
+	}
+	
+	//ws.send(messageArr[1] + ', you got ' + randInt + ' on d' + messageArr[2] ); //FOR TEST
+	ws.send(answer);
   })
-  ws.send('server_awakened')
+  ws.send('server_awakened');
 })
